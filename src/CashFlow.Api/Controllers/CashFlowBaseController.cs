dbc5ab4c12439d953c662c823ea144051f284b9e
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-06-02 06:19:03 -0300

    ✨ feat: [CashFlowBaseController] - Create a base controller to be used in each new one

---
+++ src/CashFlow.Api/Controllers/CashFlowBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CashFlowBaseController : ControllerBase
+    {
+    }
+}
