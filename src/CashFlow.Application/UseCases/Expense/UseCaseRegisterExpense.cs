df5384ecf2b84e0d60ac88857ecc07e19fb14828
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-06-03 20:54:57 -0300

    ✨ feat: [CashFlow.Application] - Create class library and first useCase

---
+++ src/CashFlow.Application/UseCases/Expense/UseCaseRegisterExpense.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Communication.Requests;
+
+namespace CashFlow.Application.UseCases.Expense
+{
+    public class UseCaseRegisterExpense
+    {
+
+        public void Execute(RequestRegisterExpenseJson request)
+        {
+            IsRequestValid(request);
+        }
+
+        private bool IsRequestValid(RequestRegisterExpenseJson request)
+        {
+            var isTitleValid = string.IsNullOrWhiteSpace(request.Title);
+
+            return true;
+        }
+
+    }
+}
