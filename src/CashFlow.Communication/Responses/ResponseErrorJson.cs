e09efca10a30c7c392bf68944f7952112e894359
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-06-07 01:32:32 -0300

    ✨ feat: [ResponseErrorJson] - Allow not only a list of errors, but a single string as well

--- src/CashFlow.Communication/Responses/ResponseErrorJson.cs
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -2,7 +2,12 @@
 {
     public class ResponseErrorJson
     {
-        public List<string> Errors { get; set; } = [];
+        public List<string> Errors { get; set; }
+
+        public ResponseErrorJson(string error)
+        {
+            Errors = [error];
+        }
 
         public ResponseErrorJson(List<string> errors)
         {
