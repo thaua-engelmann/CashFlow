8d28accf2aaa469733e03778acb85e9d39921021
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-06-07 01:09:16 -0300

    ✨ feat: [ExceptionErrorOnValidation] - Create this custom exception for validations

---
+++ CashFlow.Exception/ExceptionBase/ExceptionErrorOnValidation.cs
@@ -0,0 +1,13 @@
+﻿namespace CashFlow.Exception.ExceptionBase
+{
+    public class ExceptionErrorOnValidation : SystemException
+    {
+        public List<string> Messages { get; set; }
+
+        public ExceptionErrorOnValidation(List<string> messages)
+        {
+            Messages = messages;
+        }
+
+    }
+}
