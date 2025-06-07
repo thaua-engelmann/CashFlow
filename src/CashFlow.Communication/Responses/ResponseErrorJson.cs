9b862e243bcf3d243adc5a49ef62df7ad4e8c9bf
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-06-07 01:11:27 -0300

    ✨ feat: [ResponseErrorJson] - Create class to serve as a response error

---
+++ src/CashFlow.Communication/Responses/ResponseErrorJson.cs
@@ -0,0 +1,13 @@
+﻿namespace CashFlow.Communication.Responses
+{
+    public class ResponseErrorJson
+    {
+        public List<string> Errors { get; set; } = [];
+
+        public ResponseErrorJson(List<string> errors)
+        {
+            Errors = errors;
+        }
+
+    }
+}
