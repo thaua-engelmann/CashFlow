8abe411e3eaab4bbd3c74ba0b7badf937a1c3d43
Thauã Engelmann <engelmann.webdev@gmail.com>
2025-06-11 22:07:19 -0300

    🧪 test: [RequestRegisterExpenseBuilder] - Use Bogus package to geneate mock data

---
+++ tests/CommomTestUtilities/Requests/RequestRegisterExpenseBuilder.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+
+namespace CommomTestUtilities.Requests
+{
+    public class RequestRegisterExpenseBuilder
+    {
+
+        public RequestRegisterExpenseJson Build()
+        {
+
+            var faker = new Faker();
+
+            return new RequestRegisterExpenseJson
+            {
+                Title = faker.Commerce.ProductName(),
+                Description = faker.Commerce.ProductDescription(),
+                Date = faker.Date.Past(),
+                Amount = faker.Random.Decimal(min: 1, max: 2000),
+                PaymentType = faker.PickRandom<PaymentType>()
+            };
+        }
+
+    }
+}
