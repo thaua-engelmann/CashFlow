de4101a793ba2558c9e0bb724ad39311fd054875
Thauã Engelmann <engelmann.webdev@gmail.com>
2026-02-23 19:42:15 -0300

    ✨ feat: [UseCaseBase] - Create abstract class for each use case. Add `Validate()` method

---
+++ src/CashFlow.Application/Common/UseCaseBase.cs
@@ -0,0 +1,21 @@
+﻿using CashFlow.Exception.ExceptionBase;
+using FluentValidation;
+
+namespace CashFlow.Application.Common;
+
+public abstract class UseCaseBase
+{
+    protected void Validate<T>(IValidator<T> validator, T request)
+    {
+        var result = validator.Validate(request);
+
+        if (!result.IsValid)
+        {
+            var errors = result.Errors
+                .Select(vf => vf.ErrorMessage)
+                .ToList();
+
+            throw new ExceptionErrorOnValidation(errors);
+        }
+    }
+};
